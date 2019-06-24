e0a854e656ccbeecab4474108c54affad3b58fe8
David <davidgaona2@gmail.com>
2019-06-24 10:43:59 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    //int wSize = 100;
+    char hola[100] = {' '};
+    for (int i = 0; i < 10; ++i){
+        printf("Enter name: ");
+        scanf("%s ", hola);
+        printf("%c \n",hola[2]);
+    }
+
+    return 0;
+}
